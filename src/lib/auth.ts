5e4986d70eb5c20ec26314b48cd9a23fa468c4ed
Md Sadrul Hasan Dider <mdsadrulhasandider@gmail.com>
2026-05-02 21:21:28 +0600

    feat: initialize Better Auth configuration with MongoDB adapter and OAuth providers

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,19 +6,36 @@ const client = new MongoClient(process.env.MONGODB_URI!.trim());
 const db = client.db();
 
 export const auth = betterAuth({
-    database: mongodbAdapter(db),
-    emailAndPassword: {
-        enabled: true,
+  database: mongodbAdapter(db),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
-    socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }
+  },
+
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://programminghero-a8-qurbani-hat.vercel.app",
+  ],
+
+
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+
+  
+  cookies: {
+    sessionToken: {
+      attributes: {
+        httpOnly: true,
+        secure: true,        
+        sameSite: "none",  
+        path: "/",
+      },
     },
-    trustedOrigins: [
-        "http://localhost:3000",
-        "https://programminghero-a8-qurbani-hat.vercel.app"
-    ],
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000"
+  },
 });
